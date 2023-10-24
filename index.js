81835b2eabe8dcfb90bbb22aec601ecd818c0dcd
Colleen-L <nonexistingalien@gmail.com>
2023-10-23 23:15:05 -0400

    connection to form

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ openPopup.addEventListener("click", function(){
 closePopup.addEventListener("click", function(){
     document.body.classList.remove("active-popup");
 })
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwafaZup4LBny-MiHlWR1CqpkQzs1RM-5lOZofdvewUJ8slb4ShvE5B2m2jnt_yIDSv8w/exec'
+
+const form = document.forms['APR-Simulation']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(() => {location.replace("test.html");})
+})
